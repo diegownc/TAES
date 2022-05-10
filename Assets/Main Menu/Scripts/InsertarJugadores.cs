37db947f7a7c97f29be993c2d8db90d0e1c1fdf3
Rafael Bustos <rbm71@alu.ua.es>
2022-05-10 17:50:56 +0200

    Pequeños cambios

--- Assets/Main Menu/Scripts/InsertarJugadores.cs
+++ Assets/Main Menu/Scripts/InsertarJugadores.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Mono.Data.Sqlite;
 using System.Data;
-using System;
-using System.Threading;
 
 public class InsertarJugadores : MonoBehaviour
 {
-    private Thread PonerNombres;
+    //Thread PonerNombres;
 
     private int maximoJugadores;
+    public Text nombreDelJuego;
     public Text jugador1;
     public Text jugador2;
     public Text jugador3;
@@ -20,54 +17,68 @@ public class InsertarJugadores : MonoBehaviour
     public Text jugador6;
     private string dbName = "URI=file:Taes.db";
 
-    void Start()
+    private void nombreDeLosJuegos()
     {
-        PonerNombres = new Thread(new ThreadStart(CrearJugadores));
-        PonerNombres.Start();       
-    }
-
-    private void Update()
-    {
+        if (nombreDelJuego.text == "Extreme Football")
+        {
+            maximoJugadores = 2;
+            jugador2.text = "";
+            jugador4.text = "";
+            jugador5.text = "";
+            jugador6.text = "";
+        }
+        else
+            maximoJugadores = 6;
     }
 
     public void CrearJugadores()
     {
-        int contador = 0;
-
-        while (true)
+        using (var connection = new SqliteConnection(dbName))
         {
-            using (var connection = new SqliteConnection(dbName))
+            connection.Open();
+
+            string command = "SELECT * FROM usuarios";
+            using var cmd = new SqliteCommand(command, connection);
+
+            IDataReader reader;
+
+            using (reader = cmd.ExecuteReader())
             {
-                connection.Open();
-
-                string command = "SELECT * FROM usuarios";
-                using var cmd = new SqliteCommand(command, connection);
-
-                IDataReader reader;
-
-                using (reader = cmd.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
-                    {
-                        if (jugador1.text == "Esperando Jugador...")
-                            jugador1.text = reader["nombre"].ToString();
-                        else if(jugador2.text == "Esperando Jugador...")
-                            jugador2.text = reader["nombre"].ToString();
-                        else if (jugador3.text == "Esperando Jugador...")
-                            jugador3.text = reader["nombre"].ToString();
-                        else if (jugador4.text == "Esperando Jugador...")
-                            jugador4.text = reader["nombre"].ToString();
-                        else if (jugador5.text == "Esperando Jugador...")
-                            jugador5.text = reader["nombre"].ToString();
-                        else if (jugador6.text == "Esperando Jugador...")
-                            jugador6.text = reader["nombre"].ToString();
-                    }
+                    if (jugador1.text == "Esperando Jugador..." || jugador1.text == reader["nombre"].ToString())
+                        jugador1.text = reader["nombre"].ToString();
+                    else if ((jugador2.text == "Esperando Jugador..." || jugador2.text == reader["nombre"].ToString()) 
+                        && maximoJugadores > 2)
+                        jugador2.text = reader["nombre"].ToString();
+                    else if (jugador3.text == "Esperando Jugador..." || jugador3.text == reader["nombre"].ToString())
+                        jugador3.text = reader["nombre"].ToString();
+                    else if ((jugador4.text == "Esperando Jugador..." || jugador4.text == reader["nombre"].ToString())
+                        && maximoJugadores > 2)
+                        jugador4.text = reader["nombre"].ToString();
+                    else if ((jugador5.text == "Esperando Jugador..." || jugador5.text == reader["nombre"].ToString())
+                        && maximoJugadores > 2)
+                        jugador5.text = reader["nombre"].ToString();
+                    else if ((jugador6.text == "Esperando Jugador..." || jugador6.text == reader["nombre"].ToString())
+                        && maximoJugadores > 2)
+                        jugador6.text = reader["nombre"].ToString();
                 }
-                reader.Close();
-                connection.Close();
-
-                Thread.Sleep(2000);
             }
+
+            reader.Close();
+
+            connection.Close();
         }
+    }
+
+    void Start()
+    {
+        nombreDeLosJuegos();
+        InvokeRepeating("CrearJugadores", 0, 1);
+    }
+
+    private void Update()
+    {
+        //CrearJugadores();
     }
 }
---
+++ Assets/Main Menu/Scripts/IrALaPaginaWeb.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class IrALaPaginaWeb : MonoBehaviour
+{
+    public void Open()
+    {
+        Application.OpenURL("http://2d-party.000webhostapp.com");
+    }
+}
